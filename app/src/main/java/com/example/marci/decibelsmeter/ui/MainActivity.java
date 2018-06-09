3a3d2538ff3e784686673928f204b781d99a66a1
Marcin Uryga <marcin.uryga@onet.eu>
2018-06-09 12:14:35 +0200

    Initial commit

---
+++ app/src/main/java/com/example/marci/decibelsmeter/ui/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.marci.decibelsmeter.ui;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.marci.decibelsmeter.R;
+
+/**
+ * * Created by marci on 2018-06-09.
+ */
+
+public class MainActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(@Nullable Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+  }
+}
